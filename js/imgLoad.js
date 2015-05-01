fa9307a192c4ca795c28fab26bd26a5730e4a7fb
shine <shine130@vip.qq.com>
2015-05-01 18:29:41 +0800

    imgLoad

---
+++ js/imgLoad.js
@@ -0,0 +1,41 @@
+/**
+* @author shine
+* @blog http://blog.ipsfan.com/
+* @version 0.1
+*/ 
+(function($){
+	$.fn.imgLoad=function(options){
+		var defaults={
+			imgEle:'.imgLoad',
+			callBack:function(){}
+		};
+
+		opts=$.extend(defaults,options);
+		var img=this.find(opts.imgEle),
+			imgNum=img.length,
+			imgSrcs=[],
+			imgcounter=0;
+
+		img.each(function(){
+			if($(this).attr('src')){
+				var imgSrc=$(this).attr('src');
+				imgSrcs.push(imgSrc);
+			}else{
+				var imgSrc=($(this).css('backgroundImage')).slice(5,-2);
+				imgSrcs.push(imgSrc);
+			};
+		});
+		
+		$.each(imgSrcs,function(i,e){
+			var imgDom=$('<img>');
+			imgDom.attr('src',e);
+			imgDom.get(0).onload=function(){
+				imgcounter+=1;
+				if(imgcounter===imgNum){
+					opts.callBack();
+				};
+			};
+		});
+		
+	};
+})(jQuery)
